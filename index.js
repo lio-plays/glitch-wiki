ad05fbd46d0f453be07a20c26dea33d0510e674b
Glitch (charm-generated-screw) <none>
2020-04-05 09:39:27 +0000

    root (/) redirects to last page.

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-// url from here to glitch-editor
-const project = location.href.match(/.*\/\/(.*?)\./)[1];
-const urltag = document.querySelector("#url");
-const path = (location.pathname === "/"
-  ? "/index.html"
-  : location.pathname
-).substring(1);
+// root ("/") redirects to last page.
+//because glitch loads "/" at refresh
 
-urltag.textContent = `${project} / ${path}`;
-urltag.href = `https://glitch.com/edit/#!/${project}?path=${path}`;
+const path = location.pathname.substring(1);
+if (path === "") {
+  const path = localStorage.getItem("lastPath");
+  location.replace(path);
+} else {
+  localStorage.setItem("lastPath", path);
 
-// markdown
+  // url from here to glitch-editor
 
-const markdowntag = document.querySelector("#markdown");
-const src = markdowntag.innerHTML;
-const md = window.markdownit();
-const result = md.render(src);
-const htmltag = document.querySelector("#html");
-markdowntag.innerHTML = "";
-htmltag.innerHTML = result;
-                       
+  const project = location.href.match(/.*\/\/(.*?)\./)[1];
+  const urltag = document.querySelector("#url");
+
+  urltag.textContent = `${project} / ${path}`;
+  urltag.href = `https://glitch.com/edit/#!/${project}?path=${path}`;
+
+  // markdown
+
+  const markdowntag = document.querySelector("#markdown");
+  const src = markdowntag.innerHTML;
+  const md = window.markdownit();
+  const result = md.render(src);
+  const htmltag = document.querySelector("#html");
+  markdowntag.innerHTML = "";
+  htmltag.innerHTML = result;
+}
