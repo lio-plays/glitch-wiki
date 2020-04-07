6be808404235572204dccbbb6cce65de6577519b
Glitch (charm-generated-screw) <none>
2020-04-07 01:26:58 +0000

    Bug when lastPath === null

--- index.js
+++ index.js
@@ -1,43 +1,45 @@
-// root ("/") redirects to last page.
-// because glitch loads "/" at refresh
+(async function() {
+  // root ("/") redirects to last page.
+  // because glitch loads "/" at refresh
 
-const path = location.pathname;
-if (path === "/" && location.hash === "") {
-  const lastPath = localStorage.getItem("lastPath");
-  if (lastPath === "/") {
-    // just in case. would lead to recursion.
-    location.replace("index.html");
+  const path = location.pathname;
+  if (path === "/" && location.hash === "") {
+    const lastPath = localStorage.getItem("lastPath");
+    if (
+      lastPath === "/" || // just in case. would lead to recursion.
+      lastPath === null
+    ) {
+      location.replace("index.html");
+    } else {
+      location.replace(lastPath);
+    }
   } else {
-    location.replace(lastPath);
-  }
-} else {
-  const urltag = document.querySelector("#url");
-  const project = location.host.match(/(.*?)\./)[1];
+    const urltag = document.querySelector("#url");
+    const project = location.host.match(/(.*?)\./)[1];
 
-  //save last path, update on hashchange
+    //save last path, update on hashchange
 
-  function updateLastPath() {
-    const lastPath = location.pathname + location.hash;
-    localStorage.setItem("lastPath", lastPath);
-    urltag.textContent = `${project}${lastPath}`;
-  }
+    function updateLastPath() {
+      const lastPath = location.pathname + location.hash;
+      localStorage.setItem("lastPath", lastPath);
+      urltag.textContent = `${project}${lastPath}`;
+    }
 
-  onhashchange = () => updateLastPath();
-  updateLastPath();
+    onhashchange = () => updateLastPath();
+    updateLastPath();
 
-  // url from here to glitch-editor
+    // url from here to glitch-editor
 
-  const glitchpath =
-    path === "/" && location.hash !== ""
-      ? location.hash.substring(2)
-      : path.substring(1);
-  urltag.href = `https://glitch.com/edit/#!/${project}?path=${glitchpath}`;
+    const glitchpath =
+      path === "/" && location.hash !== ""
+        ? location.hash.substring(2)
+        : path.substring(1);
+    urltag.href = `https://glitch.com/edit/#!/${project}?path=${glitchpath}`;
 
-  // markdown
+    // markdown
 
-  const markdowntag = document.querySelector("#markdown");
-  const htmltag = document.querySelector("#html");
-  (async function() {
+    const markdowntag = document.querySelector("#markdown");
+    const htmltag = document.querySelector("#html");
     if (path === "/" && location.hash !== "") {
       htmltag.innerHTML = "<h1>Loading</h1>";
       const response = await fetch(new Request(location.hash.substring(1)));
@@ -54,5 +56,5 @@ if (path === "/" && location.hash === "") {
     const md = window.markdownit();
     const result = md.render(src);
     htmltag.innerHTML = result;
-  })();
-}
+  }
+})();
